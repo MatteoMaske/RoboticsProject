514b7145a151c801e5c3b0a8fa28ef0edeb586bc
Stefano <sacchet13@gmail.com>
2022-11-09 17:00:32 +0000

    added inverse kinematic function

--- robot_control/lab_exercises/lab_palopoli/ur5_generic.py
+++ robot_control/lab_exercises/lab_palopoli/ur5_generic.py
@@ -8,6 +8,8 @@ Created on 3 May  2022
 from __future__ import print_function
 
 import os
+
+import numpy as np
 import rospy as ros
 import sys
 # messages for topic subscribers
@@ -240,6 +242,102 @@ class Ur5Generic(BaseControllerFixed):
                     p.gm.move_gripper(100)
                 break
 
+    def numericalInverseKinematics(p_d, q0, line_search=False, wrap=False):
+        math_utils = Math()
+
+        # hyper-parameters
+        epsilon = 1e-06  # Tolerance for stropping criterion
+        lambda_ = 1e-08  # Regularization or damping factor (1e-08->0.01)
+        max_iter = 200  # Maximum number of iterations
+        # For line search only
+        gamma = 0.5
+        beta = 0.5  # Step size reduction
+
+        # initialization of variables
+        iter = 0
+        alpha = 1  # Step size
+        log_grad = []
+        log_err = []
+
+        # Inverse kinematics with line search
+        while True:
+            # evaluate  the kinematics for q0
+            J, _, _, _, _ = computeEndEffectorJacobian(q0)
+            _, _, _, _, T_0e = directKinematics(q0)
+
+            p_e = T_0e[:3, 3]
+            R = T_0e[:3, :3]
+            rpy = math_utils.rot2eul(R)
+            roll = rpy[0]
+            p_e = np.append(p_e, roll)
+
+            # error
+            e_bar = p_e - p_d
+            J_bar = geometric2analyticJacobian(J, T_0e)
+            J_bar = J_bar[:4, :]
+            # evaluate the gradient
+            grad = J_bar.T.dot(e_bar)
+
+            log_grad.append(np.linalg.norm(grad))
+            log_err.append(np.linalg.norm(e_bar))
+
+            if np.linalg.norm(grad) < epsilon:
+                print("IK Convergence achieved!, norm(grad) :", np.linalg.norm(grad))
+                print("Inverse kinematics solved in {} iterations".format(iter))
+                break
+            if iter >= max_iter:
+                print(
+                    "Warning: Max number of iterations reached, the iterative algorithm has not reached convergence to the desired precision. Error is:  ",
+                    np.linalg.norm(e_bar))
+                break
+            # Compute the error
+            JtJ = np.dot(J_bar.T, J_bar) + np.identity(J_bar.shape[1]) * lambda_
+            JtJ_inv = np.linalg.inv(JtJ)
+            P = JtJ_inv.dot(J_bar.T)
+            dq = - P.dot(e_bar)
+
+            if not line_search:
+                q1 = q0 + dq * alpha
+                q0 = q1
+            else:
+                print("Itern # :", iter)
+                # line search loop
+                while True:
+                    # update
+                    q1 = q0 + dq * alpha
+                    # evaluate  the kinematics for q1
+                    _, _, _, _, T_0e1 = directKinematics(q1)
+                    p_e1 = T_0e1[:3, 3]
+                    R1 = T_0e1[:3, :3]
+                    rpy1 = math_utils.rot2eul(R1)
+                    roll1 = rpy1[0]
+                    p_e1 = np.append(p_e1, roll1)
+                    e_bar_new = p_e1 - p_d
+                    # print "e_bar1", np.linalg.norm(e_bar_new), "e_bar", np.linalg.norm(e_bar)
+
+                    error_reduction = np.linalg.norm(e_bar) - np.linalg.norm(e_bar_new)
+                    threshold = 0.0  # more restrictive gamma*alpha*np.linalg.norm(e_bar)
+
+                    if error_reduction < threshold:
+                        alpha = beta * alpha
+                        print(" line search: alpha: ", alpha)
+                    else:
+                        q0 = q1
+                        alpha = 1
+                        break
+
+            iter += 1
+
+        # wrapping prevents from outputs outside the range -2pi, 2pi
+        if wrap:
+            for i in range(len(q0)):
+                while q0[i] >= 2 * math.pi:
+                    q0[i] -= 2 * math.pi
+                while q0[i] < -2 * math.pi:
+                    q0[i] += 2 * math.pi
+
+        return q0, log_err, log_grad
+
 def talker(p):
     p.start()
     if p.real_robot:
@@ -274,6 +372,9 @@ def talker(p):
 
         if p.homing_flag:
             p.homing_procedure(conf.robot_params[p.robot_name]['dt'], 0.6, conf.robot_params[p.robot_name]['q_0'], rate)
+
+            ##move to brick1
+            #p.numericalInverseKinematics(np.array([0.64, 0.6, 0.925 ]), conf.robot_params[p.robot_name]['q_0'])
 
         ## set joints here
         #p.q_des = p.q_des_q0  + 0.1 * np.sin(2*np.pi*0.5*p.time)
