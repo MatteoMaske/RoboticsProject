2ed2480baa2bfce6f29df20d2c518191fbd220a4
Amir <amir.gheser@studenti.unitn.it>
2022-11-13 18:13:24 +0100

    Creation of vision.py file

---
+++ vision.py
@@ -0,0 +1,12 @@
+# config interpreter to python 3
+
+# main
+
+if __name__ == '__main__':
+    print("Hello")
+    # train the yolov5 model
+    
+    # configure this node so that it waits til it receives an input
+    # On input received call yolo to fetch data
+    # Output label
+    # Sì lo so non serve a molto, però ho fatto finta di fare qualcosa
