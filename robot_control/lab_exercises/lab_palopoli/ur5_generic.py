18d5afcf9c51de21b6e18f54a521c9e774691d15
Stefano <sacchet13@gmail.com>
2022-11-15 13:32:47 +0000

    prova

--- robot_control/lab_exercises/lab_palopoli/ur5_generic.py
+++ robot_control/lab_exercises/lab_palopoli/ur5_generic.py
@@ -320,8 +320,6 @@ def talker(p):
         rate.sleep()
         p.time = np.round(p.time + np.array([conf.robot_params[p.robot_name]['dt']]),  3)  # to avoid issues of dt 0.0009999
 
-        print("Dio boia")
-
 if __name__ == '__main__':
 
     p = Ur5Generic(robotName)
