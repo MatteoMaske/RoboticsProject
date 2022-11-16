0ce981d35ad8111d2d6187df6d37873bea4d217f
Amir <amir.gheser@studenti.unitn.it>
2022-11-16 15:48:41 +0100

    modifica da poco

---
+++ cpp_scripts/move.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <eigen.h>
+
+int main(){
+
+
+
+    return 0;
+}
+
+void inverseKinematics(){
+
+}
+
+void forwardKinematics(){
+
+}
