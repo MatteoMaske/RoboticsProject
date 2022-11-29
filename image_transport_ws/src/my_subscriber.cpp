f71a68002b8737a20389115cf36d4ea482989373
MatteoMaske <matteomascherin@gmail.com>
2022-11-29 14:48:45 +0100

    stl of the megablock added in models dir and ur5 modified with tavolo_megablock world

---
+++ image_transport_ws/src/my_subscriber.cpp
@@ -0,0 +1,34 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+  try
+  {
+    cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
+    cv::waitKey(30);
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+  }
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "image_listener");
+  ros::NodeHandle nh;
+  cv::namedWindow("view");
+
+  cout << "bella zi" << endl;
+
+  image_transport::ImageTransport it(nh);
+  image_transport::Subscriber sub = it.subscribe("z_base_camera/camera/rgb/image_raw", 1, imageCallback);
+  ros::spin();
+  cv::destroyWindow("view");
+}
