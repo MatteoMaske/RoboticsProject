d072fb0f6de1089750991a5d7ba460178cb6fc47
Amir <amir.gheser@studenti.unitn.it>
2022-11-30 15:16:09 +0100

    changed to radians

--- models/screenShotScript.py
+++ models/screenShotScript.py
@@ -90,7 +90,7 @@ class Render:
                     for gamma in range(self.gamma_limits[0], self.gamma_limits[1] + 1, rotation_step): # Loop to vary the angle gamma
                         render_counter += 1 # Update counter
                         for _ in range(4):
-                            self.objetcs[0].rotation_euler = (90, 0, 0)
+                            self.objetcs[0].rotation_euler = (m.radians(90), 0, 0)
                             ## Update the rotation of the axis
                             axis_rotation = (m.radians(beta_r), 0, m.radians(gamma)) 
                             self.axis.rotation_euler = axis_rotation # Assign rotation to <bpy.data.objects['Empty']> object
