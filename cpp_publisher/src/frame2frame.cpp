ed5d5c9d942ac470f1a7aa6be098d6aed2262f70
MatteoMaske <matteomascherin@gmail.com>
2022-12-16 12:00:48 +0100

    adding bugs

--- cpp_publisher/src/frame2frame.cpp
+++ cpp_publisher/src/frame2frame.cpp
@@ -4,24 +4,52 @@
 #include <vector>
 #include <Eigen/Dense>
 
+using namespace std;
+
 typedef double radiants;
 typedef Eigen::Matrix<double, 3, 3> Matrix3d;
 typedef Eigen::Matrix<double, 3, 1> Vector3d;
 typedef Eigen::Matrix<double, 4, 4> Matrix4d;
 typedef Matrix3d RotationMatrix;
 
+Vector3d fromWorldToBase(Vector3d point);
+Matrix4d transformPoint(Vector3d pointA, Vector3d pointB, vector<radiants> theta);
+Matrix4d transformPointAux(Vector3d translation, vector<radiants> theta);
+
 // TODO TEST THE FUNCTION
 int main(){
-    // call function in here
-    // do testing in here
+
+    Vector3d point;
+    point << 0.8,0.45,0.9;
+    cout << fromWorldToBase(point) << endl;
+
+
+    return 0;
+    
 }
 
-Matrix4d transformPoint(Vector3d pointA, Vector3d pointB, std::vector<radiants> theta){
+Vector3d fromWorldToBase(Vector3d point){
+
+    //taken in input a point in the world frame, it returns the point in the base frame knowing 
+    //the position of the base frame in the world frame and the orientation of the base frame in the world frame
+    //is 0.5, 0.35, 1.75 , pi, 0, 0
+
+    Vector3d pointA;
+    pointA << 0.5, 0.35, 1.75;
+    vector<radiants> theta = {0, 0, M_PI};
+
+    Matrix4d transformPointMat = transformPoint(pointA, point, theta);
+    Vector3d pointB;
+    pointB << transformPointMat(0,3), transformPointMat(1,3), transformPointMat(2,3);
+    return pointB;
+}
+
+Matrix4d transformPoint(Vector3d pointA, Vector3d pointB, vector<radiants> theta){
     Vector3d translation = pointB - pointA;
-    return transformPoint(translation, theta);
+    return transformPointAux(translation, theta);
 }
 
-Matrix4d transformPoint(Vector3d translation, std::vector<radiants> theta){
+Matrix4d transformPointAux(Vector3d translation, vector<radiants> theta){
     RotationMatrix Rx;
     Rx << 1, 0, 0,
           0, cos(theta[0]), -sin(theta[0]),
