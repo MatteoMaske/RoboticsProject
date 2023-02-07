1109a36c688b7195272d787e212525e066796aa8
Stefano <sacchet13@gmail.com>
2023-02-07 17:34:01 +0100

    syncronized yolo detection and zed point_cloud

--- py_publisher/src/vision.py
+++ py_publisher/src/vision.py
@@ -5,8 +5,8 @@ import cv2
 import numpy as np
 import rospy
 from std_msgs.msg import String
-import sensor_msgs.msg
 from PIL import Image
+import sensor_msgs.msg
 from cv_bridge import CvBridge
 from py_publisher.msg import BlockDetected #custom messages
 from py_publisher.msg import BlockInfo
@@ -20,7 +20,9 @@ import message_filters
 """
 TODO:
     - implement a function that generates unique id
-    - check if the z is right
+    - if pixels don't change, don't publish
+    done - check if the z is right [0.85 - 0.90]
+    - crop the image to see only the table
 """
 
 # result.boxes.xyxy   # box with xyxy format, (N, 4)
@@ -30,28 +32,18 @@ TODO:
 # result.boxes.conf   # confidence score, (N, 1)
 # result.boxes.cls    # cls, (N, 1)
 
-"""
-Matrix to store data for each block:
-
-          id  class
-block1    0    0
-block2    1    10
-block3    8    80
-"""
-
 ZED_LEFT_TOPIC = "/ur5/zed_node/left/image_rect_color"
-ZED_DEPTH_TOPIC = "/ur5/zed_node/depth/depth_registered"
+ZED_POINT_CLOUD_TOPIC = "/ur5/zed_node/point_cloud/cloud_registered"
+# ZED_DEPTH_TOPIC = "/ur5/zed_node/depth/depth_registered"
 SLEEP_RATE = 10
 
 WEIGHT = '/home/stefano/ros_ws/src/visionData/best.pt'
-IMAGE = '/home/stefano/ros_ws/src/visionData/1.jpg'
+# IMAGE = '/home/stefano/ros_ws/src/visionData/1.jpg'
 
 IMGSZ = 1280
 
-matrix = np.empty((0,4))
-
-prevX, prevY = 0,0 #previous point
-pointX, pointY = 0,0 #current point
+MIN_Z = 0.85
+MAX_Z = 0.90
 
 def findCenter(result):
     block = BlockInfo()
@@ -68,181 +60,129 @@ def findCenter(result):
     y = int((y1 + y2) / 2)
 
     blockClass = int(result.boxes.cls.tolist()[0])
-    global matrix
-    #TODO: check if the block is already in the matrix
-    #TODO: write getID(blockClass) function
-    matrix = np.vstack((matrix, [0, blockClass, x, y]))
 
-    #print("xx: ", x)
-    #print("yy: ", y)
+    info = {
+        'id': 0,
+        'class': blockClass,
+        'x': x,
+        'y': y
+    }
 
-    global pointX, pointY, prevX, prevY
-    prevX, prevY = pointX, pointY #save the previous point
-    pointX, pointY = x, y #save the current point
+    return info
 
 def detect(image):
     model = YOLO(WEIGHT)
-    results = model.predict(source=image, imgsz=IMGSZ, stream=True)
-    for result in results: #need to make a better check on empty results
-        if not len(result) == 0:
-            findCenter(result)
-        else:
-            print("No blocks detected")
+    results = model.predict(source=image, imgsz=IMGSZ)
+    list = []
+    if not (len(results[0]) == 0):
+        for i in range(len(results[0])):
+            list.append(findCenter(results[0][i]))
+    else:
+        print("No blocks detected")
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id())
-    bridge = CvBridge()
-    image = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
-    # cv2.imshow('image',image)
-    # cv2.waitKey(0)
-    #cropImage() # need to crop the table
-    cv2.imwrite('img.jpeg',image)
-    detect(image)
+    # print("List:\n", list)
+    return list
 
-def builMessage(pointW):
+def buildMsg(listCoord):
 
     msg = BlockDetected()
 
-    block = BlockInfo()
-    block.id = Int16(0)
-    block.objectClass = Byte(0)
-    block.position = Point(pointW[0],pointW[1],pointW[2])
-    msg.blockDetected.append(block)
+    for i in range(len(listCoord)):
+        block = BlockInfo()
+        block.id = Int16(listCoord[i]['id'])
+        block.objectClass = Byte(listCoord[i]['class'])
+        block.position = Point(listCoord[i]['x'], listCoord[i]['y'], listCoord[i]['z'])
+        msg.blockDetected.append(block)
 
-    # for i in range(matrix.shape[0]):
-    #     block = BlockInfo()
-    #     block.id = Int16(matrix[i][0])
-    #     block.objectClass = Byte(matrix[i][1])
-    #     block.position = Point(pointW[0],pointW[1],pointW[2])
-    #     msg.blockDetected.append(block)
-    
-    #print("Message:\n", msg)
-    talker(msg)
+    print("Message:\n", msg)
+    return msg
 
-def receivePointcloud(msg):
+def receivePointcloud(msg, list):
     points_list = []
+    listCoord = {
+        'id': list['id'],
+        'class': list['class'],
+        'x': 0,
+        'y': 0,
+        'z': 0
+    }
 
-    print("X: ",pointX)
-    print("Y: ", pointY)
-    print("prevX: ", prevX)
-    print("prevY: ", prevY)
+    x = list['x']
+    y = list['y']
 
     #if the point didn't change, don't do anything
-    if not (abs(pointX - prevX) < 10 and abs(pointY - prevY) < 10):
+    # if not (abs(x - prevX) < 10 and abs(y - prevY) < 10): # need to check if the point didn't changed
 
-        for data in point_cloud2.read_points(msg, field_names=['x','y','z'], skip_nans=False, uvs=[(pointX, pointY)]):
-            points_list.append([data[0], data[1], data[2]])
+    for data in point_cloud2.read_points(msg, field_names=['x','y','z'], skip_nans=False, uvs=[(x, y)]):
+        points_list.append([data[0], data[1], data[2]]) #coordinates in the camera frame
 
-        #print("Data Optical frame: ", points_list)
+    #black magic
+    base_offset = [0.5,0.35,1.75]
+    x_c = [-0.9,0.24,-0.35]
 
-        #black magic
-        base_offset = [0.5,0.35,1.75]
-        x_c = [-0.9,0.24,-0.35]
+    rotation = np.array([[0., -0.49948, 0.86632],
+                        [-1., 0., 0.],
+                        [-0., -0.86632, -0.49948]])
+    
+    #coordinates in the world frame
+    pointW = rotation.dot(points_list[0]) + x_c + base_offset
+    # print("World coord: ", pointW)
 
-        rotation = np.array([[0., -0.49948, 0.86632],
-                            [-1., 0., 0.],
-                            [-0., -0.86632, -0.49948]])
-        
-        #coordinates in the world frame
-        pointW = rotation.dot(points_list[0]) + x_c + base_offset
-        print("World coord: ", pointW)
-
-        #check if the point is valid
-        if not (math.isnan(pointW[0]) and math.isnan(pointW[1]) and math.isnan(pointW[2])):
-            builMessage(pointW)
-        else:
-            print("Nan detected")
+    #check if the point is a NaN
+    if not (math.isnan(pointW[0]) and math.isnan(pointW[1]) and math.isnan(pointW[2])):
+        # buildMsg(pointW)
+        listCoord['x'] = pointW[0]
+        listCoord['y'] = pointW[1]
+        listCoord['z'] = pointW[2]
     else:
-        print("Point not changed")
+        print("Nan detected")
+    
+    # else:
+    #     print("Point not changed")
+
+    return listCoord
 
 def talker(msg):
     rospy.init_node('publisher',anonymous=True)
-    pub = rospy.Publisher('vision/vision_detection', BlockDetected, queue_size=100)
-
-    # block = BlockInfo()
-    # block.id = Int16(1)
-    # block.objectClass = Byte(1)
-    # block.position = Point(0.1,0.45,0.9)
-
-    # block1 = BlockInfo()
-    # block1.id = Int16(2)
-    # block1.objectClass = Byte(1)
-    # block1.position = Point(0.1,0.65,0.9)
-
-    # msg = BlockDetected()
-    # msg.blockDetected.append(block)
-    # msg.blockDetected.append(block1)
+    pub = rospy.Publisher('vision/vision_detection', BlockDetected, queue_size=100) #publish to planner
     
     print("Publishing message:\n", msg)
-
     pub.publish(msg)
 
-def callback1(img, pointCloud):
+def callback(img, pointCloud):
+    #convert image to cv2
     bridge = CvBridge()
     image = bridge.imgmsg_to_cv2(img, desired_encoding='passthrough')
-    # cv2.imshow('image',image)
-    # cv2.waitKey(0)
-    #cropImage() # need to crop the table
-    detect(image)
+
+    #need to crop the image to get only the table
+    # cropImage()
+
+    #detect blocks with YOLO
+    list = detect(image)
+
+    #get coordinates from x,y pixels
+    listCoord = []
+    for i in range(len(list)):
+        if list[i]['z'] >= MIN_Z or list[i]['z'] <= MAX_Z:
+            listCoord.append(receivePointcloud(pointCloud, list[i]))
+        else:
+            print("Block out of z range")
+
+    #build the massage to publish it
+    msg = buildMsg(listCoord)
+    # print("Message:\n", msg)
+    
+    #publish the message
+    talker(msg)
 
 if __name__ == '__main__':
 
     rospy.init_node('publisher',anonymous=True)
 
-    # rospy.Subscriber(ZED_LEFT_TOPIC, sensor_msgs.msg.Image, callback) #subscribe to zed image
-    # rospy.Subscriber("/ur5/zed_node/point_cloud/cloud_registered", PointCloud2, receivePointcloud) #subscribe to zed point cloud
-
     imageSub = message_filters.Subscriber(ZED_LEFT_TOPIC, sensor_msgs.msg.Image) #subscribe to zed image
-    pointCloudSub = message_filters.Subscriber("/ur5/zed_node/point_cloud/cloud_registered", PointCloud2) #subscribe to zed point cloud
+    pointCloudSub = message_filters.Subscriber(ZED_POINT_CLOUD_TOPIC, PointCloud2) #subscribe to zed point cloud
     ts = message_filters.TimeSynchronizer([imageSub, pointCloudSub], 1)
-    ts.registerCallback(callback1)
+    ts.registerCallback(callback)
 
     while not rospy.is_shutdown():
         rospy.sleep(SLEEP_RATE)
-
-############################################################################################
-"""
-Matrix to store data for each block:
-
-        class   confidence    box
-block1    0       0.9999    (x, y)
-block2    1       0.4678    (x, y)
-block3    8       0.6534    (x, y)
-"""
-
-# model = YOLO('best.pt')
-
-# results = model.predict(source='1.jpg', imgsz=IMGSZ, stream=True)
-
-# matrix = np.empty((0,4))
-
-# for result in results:
-
-#     blockClass = int(result.boxes.cls.tolist()[0])
-#     confidence = round(result.boxes.conf.tolist()[0],4)
-
-#     #print("Block: " + str(i) + "\n   Class: " + str(blockClass) + "\n   Confidence: " + str(confidence))
-
-#     x1 = int(result.boxes.xyxy[0][0])
-#     y1 = int(result.boxes.xyxy[0][1])
-#     x2 = int(result.boxes.xyxy[0][2])
-#     y2 = int(result.boxes.xyxy[0][3])
-
-#     #find the center of the square
-#     x = int((x1 + x2) / 2)
-#     y = int((y1 + y2) / 2)
-
-#     matrix = np.vstack((matrix, [blockClass, confidence, x, y]))
-#     #print(matrix)
-
-# img = cv2.imread('1.jpg')
-# for i in range(matrix.shape[0]):
-
-#     x = int(matrix[i][2])
-#     y = int(matrix[i][3])
-
-#     img = cv2.circle(img, (x, y), 5, (255, 0, 0), -1)
-
-# cv2.imshow('image', img)
-# cv2.waitKey(0)
-############################################################################################
--- vision/py_scripts/tmp.py
+++ vision/py_scripts/tmp.py
@@ -1,6 +1,59 @@
 import cv2
+from ultralytics import YOLO
 
-img = cv2.imread('img.jpeg')
-img = cv2.circle(img, (922, 643), 5, (255, 0, 0), -1)
-cv2.imshow('img',img)
-cv2.waitKey(0)
+model = YOLO('best.pt')
+results = model.predict(source='1.png', imgsz=1571)
+print("******")
+print(results)
+print("******")
+print(results[0])
+print("******")
+print(len(results[0]))
+
+if not (len(results[0]) == 0):
+    print("Dio cane")
+
+# for i in range (len(results[0].boxes.xyxy)):
+#     print(results[0].boxes.xyxy[i])
+
+# img = cv2.imread('img.jpeg')
+# img = cv2.circle(img, (922, 643), 5, (255, 0, 0), -1)
+# cv2.imshow('img',img)
+# cv2.waitKey(0)
+
+# model = YOLO('best.pt')
+
+# results = model.predict(source='1.jpg', imgsz=IMGSZ, stream=True)
+
+# matrix = np.empty((0,4))
+
+# for result in results:
+
+#     blockClass = int(result.boxes.cls.tolist()[0])
+#     confidence = round(result.boxes.conf.tolist()[0],4)
+
+#     #print("Block: " + str(i) + "\n   Class: " + str(blockClass) + "\n   Confidence: " + str(confidence))
+
+#     x1 = int(result.boxes.xyxy[0][0])
+#     y1 = int(result.boxes.xyxy[0][1])
+#     x2 = int(result.boxes.xyxy[0][2])
+#     y2 = int(result.boxes.xyxy[0][3])
+
+#     #find the center of the square
+#     x = int((x1 + x2) / 2)
+#     y = int((y1 + y2) / 2)
+
+#     matrix = np.vstack((matrix, [blockClass, confidence, x, y]))
+#     #print(matrix)
+
+# img = cv2.imread('1.jpg')
+# for i in range(matrix.shape[0]):
+
+#     x = int(matrix[i][2])
+#     y = int(matrix[i][3])
+
+#     img = cv2.circle(img, (x, y), 5, (255, 0, 0), -1)
+
+# cv2.imshow('image', img)
+# cv2.waitKey(0)
+############################################################################################
