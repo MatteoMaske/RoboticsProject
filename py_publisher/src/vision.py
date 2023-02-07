fb4057c304e4de9b0cd4101ad0cd13505221c061
Stefano <sacchet13@gmail.com>
2023-02-07 11:20:07 +0100

    adding publishing controls

--- py_publisher/src/vision.py
+++ py_publisher/src/vision.py
@@ -14,6 +14,7 @@ from std_msgs.msg import Byte, Int16
 from geometry_msgs.msg import Point
 from sensor_msgs.msg import PointCloud2
 from sensor_msgs import point_cloud2
+import math
 
 # result.boxes.xyxy   # box with xyxy format, (N, 4)
 # result.boxes.xywh   # box with xywh format, (N, 4)
@@ -21,6 +22,15 @@ from sensor_msgs import point_cloud2
 # result.boxes.xywhn  # box with xywh format but normalized, (N, 4)
 # result.boxes.conf   # confidence score, (N, 1)
 # result.boxes.cls    # cls, (N, 1)
+
+"""
+Matrix to store data for each block:
+
+          id  class
+block1    0    0
+block2    1    10
+block3    8    80
+"""
 
 ZED_LEFT_TOPIC = "/ur5/zed_node/left/image_rect_color"
 ZED_DEPTH_TOPIC = "/ur5/zed_node/depth/depth_registered"
@@ -31,7 +41,10 @@ IMAGE = '/home/stefano/ros_ws/src/visionData/1.jpg'
 
 IMGSZ = 1280
 
-pointX, pointY = 0,0
+matrix = np.empty((0,2))
+
+prevX, prevY = 0,0 #previous point
+pointX, pointY = 0,0 #current point
 
 def findCenter(result):
     block = BlockInfo()
@@ -47,18 +60,21 @@ def findCenter(result):
     x = int((x1 + x2) / 2)
     y = int((y1 + y2) / 2)
 
-    print("xx: ", x)
-    print("yy: ", y)
+    blockClass = int(result.boxes.cls.tolist()[0])
+    global matrix
+    matrix = np.vstack((matrix, [0, blockClass]))
 
-    global pointX
-    global pointY
-    pointX = x
-    pointY = y
+    #print("xx: ", x)
+    #print("yy: ", y)
+
+    global pointX, pointY, prevX, prevY
+    prevX, prevY = pointX, pointY #save the previous point
+    pointX, pointY = x, y #save the current point
 
 def detect(image):
     model = YOLO(WEIGHT)
     results = model.predict(source=image, imgsz=IMGSZ, stream=True)
-    for result in results:
+    for result in results: #need to make a better check on empty results
         if not len(result) == 0:
             findCenter(result)
         else:
@@ -74,53 +90,85 @@ def callback(data):
     cv2.imwrite('img.jpeg',image)
     detect(image)
 
-def talker():
-    rospy.init_node('publisher',anonymous=True)
-    pub = rospy.Publisher('vision/vision_detection', BlockDetected, queue_size=100)
-
-    block = BlockInfo()
-    block.id = Int16(1)
-    block.objectClass = Byte(1)
-    block.position = Point(0.1,0.45,0.9)
-
-    block1 = BlockInfo()
-    block1.id = Int16(2)
-    block1.objectClass = Byte(1)
-    block1.position = Point(0.1,0.65,0.9)
+def builMessage(pointW):
 
     msg = BlockDetected()
+
+    block = BlockInfo()
+    block.id = Int16(0)
+    block.objectClass = Byte(0)
+    block.position = Point(pointW[0],pointW[1],pointW[2])
     msg.blockDetected.append(block)
-    msg.blockDetected.append(block1)
+
+    # for i in range(matrix.shape[0]):
+    #     block = BlockInfo()
+    #     block.id = Int16(matrix[i][0])
+    #     block.objectClass = Byte(matrix[i][1])
+    #     block.position = Point(pointW[0],pointW[1],pointW[2])
+    #     msg.blockDetected.append(block)
     
-    # while not rospy.is_shutdown():
-    #     pub.publish(msg)
-    #     rospy.sleep(SLEEP_RATE)
+    #print("Message:\n", msg)
+    talker(msg)
 
 def receivePointcloud(msg):
     points_list = []
-    #this is the center of the image plane
-    # center_x = int(msg.width / 2)
-    # center_y = int(msg.height / 2)
 
     print("X: ",pointX)
     print("Y: ", pointY)
+    print("prevX: ", prevX)
+    print("prevY: ", prevY)
 
-    for data in point_cloud2.read_points(msg, field_names=['x','y','z'], skip_nans=False, uvs=[(pointX, pointY)]):
-        points_list.append([data[0], data[1], data[2]])
+    if not (abs(pointX - prevX) < 10 and abs(pointY - prevY) < 10):
 
-    #print("Data Optical frame: ", points_list)
+        for data in point_cloud2.read_points(msg, field_names=['x','y','z'], skip_nans=False, uvs=[(pointX, pointY)]):
+            points_list.append([data[0], data[1], data[2]])
 
-    #black magic
-    base_offset = [0.5,0.35,1.75]
-    x_c = [-0.9,0.24,-0.35]
+        #print("Data Optical frame: ", points_list)
 
-    rotation = np.array([[0., -0.49948, 0.86632],
- 	                     [-1., 0., 0.],
- 	                     [-0., -0.86632, -0.49948]])
+        #black magic
+        base_offset = [0.5,0.35,1.75]
+        x_c = [-0.9,0.24,-0.35]
+
+        rotation = np.array([[0., -0.49948, 0.86632],
+                            [-1., 0., 0.],
+                            [-0., -0.86632, -0.49948]])
+        
+        #coordinates in the world frame
+        pointW = rotation.dot(points_list[0]) + x_c + base_offset
+        print("World coord: ", pointW)
+
+        #check if the point is valid
+        if not (math.isnan(pointW[0]) and math.isnan(pointW[1]) and math.isnan(pointW[2])):
+            builMessage(pointW)
+        else:
+            print("Nan detected")
+    else:
+        print("Point not changed")
+
+def talker(msg):
+    rospy.init_node('publisher',anonymous=True)
+    pub = rospy.Publisher('vision/vision_detection', BlockDetected, queue_size=100)
+
+    # block = BlockInfo()
+    # block.id = Int16(1)
+    # block.objectClass = Byte(1)
+    # block.position = Point(0.1,0.45,0.9)
+
+    # block1 = BlockInfo()
+    # block1.id = Int16(2)
+    # block1.objectClass = Byte(1)
+    # block1.position = Point(0.1,0.65,0.9)
+
+    # msg = BlockDetected()
+    # msg.blockDetected.append(block)
+    # msg.blockDetected.append(block1)
     
-    #coordinates in the world frame
-    pointW = rotation.dot(points_list[0]) + x_c + base_offset
-    print("World: ", pointW)
+    print("Publishing message:\n", msg)
+    pub.publish(msg)
+
+    # while not rospy.is_shutdown():
+    #     pub.publish(msg)
+    #     rospy.sleep(SLEEP_RATE)
 
 if __name__ == '__main__':
 
